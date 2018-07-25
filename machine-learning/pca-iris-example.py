b3ca9a45b631876ce83b1d4dbcab5e061ba8571f
whackadoodle <sundaresan.aravind2@gmail.com>
2018-07-25 23:23:57 +0530

    Apples vs Oranges classifier using Decision Trees

--- machine-learning/pca-iris-example.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load the Iris dataset
-iris_df = pd.read_csv(filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-
-iris_df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
-print(iris_df.head())
